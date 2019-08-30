d9cf7d50862cca0c56b84b047bd625cffab4cda1
Mark Faulkner <markfaulk350@gmail.com>
2019-08-30 16:17:28 -0700

    added terraform code to save tfstate file to s3 bucket

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -16,3 +16,12 @@ resource "aws_lambda_function" "TrackPilotsAPI" {
   }
  }
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "iac-trackpilots"
+    key    = "terraform/backend/trackpilots/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
