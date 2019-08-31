9d26d4138d9086b7d5879194ba754c430952f52f
Mark Faulkner <markfaulk350@gmail.com>
2019-08-30 19:59:47 -0700

    troubleshooting with terraform

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,23 +1,3 @@
-resource "aws_lambda_function" "TrackPilotsAPI" {
-  function_name = "TrackPilotsAPI"
-  handler = "main"
-  filename = "../../main.zip"
-  source_code_hash = "${filebase64sha256("../../main.zip")}"
-  runtime = "go1.x"
-  timeout = "30"
-  role = "${aws_iam_role.TrackPilotsAPI_lambda.arn}"
-
-  environment {
-      variables = {
-      DB_NAME = "${var.DB_NAME}"
-      DB_PASSWORD = "${var.DB_PASSWORD}"
-      DB_SESSION = "${var.DB_SESSION}"
-      DB_USER = "${var.DB_USER}"
-  }
- }
-}
-
-
 terraform {
   backend "s3" {
     bucket = "iac-trackpilots"
